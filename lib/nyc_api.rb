98abfc46e2bb7ebb2ebd8f3f3fafe1aab69b28be
Alex <alexfrancisaaron@gmail.com>
2020-12-28 14:12:08 -0600

    commit tree 1458c7f6009d4e5b42548f24dbd7237eec4bd8d0

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -1,13 +1,14 @@
 require 'net/http'
 require 'open-uri'
 require 'json'
- 
+require 'pry'
 class GetPrograms
 
   URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
   def get_programs
     uri = URI.parse(URL)
+    binding.pry
     response = Net::HTTP.get_response(uri)
     response.body
   end
